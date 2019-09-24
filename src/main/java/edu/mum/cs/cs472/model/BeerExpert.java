462fd4e26d6a02e7d7d2a4b588c721ca2fe49393
eduke <iameden.84@gmail.com>
2019-09-23 22:52:12 -0500

    fcommit

---
+++ src/main/java/edu/mum/cs/cs472/model/BeerExpert.java
@@ -0,0 +1,22 @@
+package edu.mum.cs.cs472.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeerExpert {
+    public BeerExpert() {
+    }
+
+    public List getBrands(String var1) {
+        ArrayList var2 = new ArrayList();
+        if (var1.equals("amber")) {
+            var2.add("Jack Amber");
+            var2.add("Red Moose");
+        } else {
+            var2.add("Jail Pale Ale");
+            var2.add("Gout Stout");
+        }
+
+        return var2;
+    }
+}
